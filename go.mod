243c655a3965f4db6605104ef052d223787c5cf9
Jules Morrison <julian.morrison@gmail.com>
2020-07-20 00:49:01 +0100

    Try a module, go-cmp

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/snarglebritchet/playing
 
 go 1.13
+
+require github.com/google/go-cmp v0.5.0
--- playing.go
+++ playing.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
